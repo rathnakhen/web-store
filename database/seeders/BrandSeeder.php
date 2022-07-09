4cc8224a5d5bf60a1ebbbbbdb511c3135a3fa80d
unknown <rathnakhen@gmail.com>
2022-07-09 16:27:40 +0700

    first git is hard

--- database/seeders/BrandSeeder.php
+++ database/seeders/BrandSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Brand;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,8 @@ class BrandSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $lv = Brand::create([
+            'name' => 'LV'
+        ]);
     }
 }
--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Brand;
+use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +17,15 @@ class ProductSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $bag = Product::create([
+           'name' => 'Sling Bag',
+//           'category_id' => Category::factory(), //only use for factory
+            'category_id' => '1',
+            'brand_id' => '1',
+            'price' => '500',
+            'sku' => '12042556',
+            'description' => 'Virtual Louis Vuitton Sling Bag For Men Shop, 60% OFF',
+            'image' => '1657356358-Sling-Bag.jpg'
+        ]);
     }
 }
